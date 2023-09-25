855aaf73afa73bcbbbf48c24e87cf80573b24279
acer <acer@TMP249-G2-M.bbrouter>
2023-09-25 17:04:03 +0530

    first commit

---
+++ project1/src/com/tns/project1/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.project1;
+
+public class Encapsulation {
+	//data members
+	public int serialno;
+	public String name;
+	public int age;
+	//member function
+	public void show()
+	{
+		System.out.println("serialno"+serialno+"\t"+"name"+name+"\t"+"age"+age+"\t");
+			}
+
+}
---
+++ project1/src/com/tns/project1/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.tns.project1;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Encapsulation ob=new Encapsulation();//created object
+      ob.serialno=49;
+      ob.name="kusuma";
+      ob.age=22;
+      ob.show();
+	}
+	}
