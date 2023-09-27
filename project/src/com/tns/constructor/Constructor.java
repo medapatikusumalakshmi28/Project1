73a33bbe9d8977b943ca809a30d92879c65f9646
acer <acer@TMP249-G2-M.bbrouter>
2023-09-27 09:18:27 +0530

    second program

---
+++ project/src/com/tns/constructor/Constructor.java
@@ -0,0 +1,15 @@
+package com.tns.constructor;
+
+public class Constructor {
+	//data members
+	public int serialno;
+	public String name;
+	public int age;
+	//member functions
+	public void show()
+	{
+		System.out.println("serialno"+serialno+"\t"+"name"+name+"\t"+"age"+age+"\t");
+	}
+	}
+
+
