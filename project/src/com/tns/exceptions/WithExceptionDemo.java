15d39017ab12866fcea6855138af43aa28817b5d
acer <acer@TMP249-G2-M.bbrouter>
2023-10-12 10:01:24 +0530

    exception commit

---
+++ project/src/com/tns/exceptions/WithExceptionDemo.java
@@ -0,0 +1,8 @@
+package com.tns.exceptions;
+
+public class WithExceptionDemo {
+	public static void main(String[] args) {
+		WithException.divide();
+	}
+
+}
---
+++ project/src/com/tns/exceptions/WithoutException.java
@@ -0,0 +1,10 @@
+package com.tns.exceptions;
+
+public class WithoutException {
+	public static void main(String[] args) {
+		int d=0;
+		int a=45/d;
+		System.out.println("will it be printed");
+		}
+
+}
