855b925a8d5241300d92843ec6283673bcec18e5
acer <acer@10.0.0.117>
2023-10-27 14:16:00 +0530

    lambda commit1

---
+++ project/src/com/tns/lambda/LambdaCollections.java
@@ -0,0 +1,17 @@
+package com.tns.lambda;
+
+import java.util.ArrayList;
+
+public class LambdaCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer>li=new ArrayList<>();
+		li.add(10);
+		li.add(50);
+		li.add(20);
+		li.add(660);
+		li.forEach(element->System.out.println(element));
+		}
+
+	}
